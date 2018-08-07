499d7963f4074fbdd7a05a1866b6061080a57020
Ryan Sand <rsand3642@gmail.com>
2018-08-07 15:09:43 -0400

    removed build directory

--- build/accelerator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/r09/xacc-atos/accelerator
-# Build directory: /home/r09/xacc-atos/build/accelerator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/r09/xacc-atos/tests
-# Build directory: /home/r09/xacc-atos/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xacc_ATOSAcceleratorTester "/home/r09/xacc-atos/build/tests/ATOSAcceleratorTester")
-add_test(xacc_ATOSVisitorTester "/home/r09/xacc-atos/build/tests/ATOSVisitorTester")
